a8ef541de74952f88772214abea5fa12efdd4033
Andrei Khodko <andrei.khodko@gmail.com>
2022-06-16 14:06:35 +0300

    Add CORS into webhost app

--- cmd/webhost/main.go
+++ cmd/webhost/main.go
@@ -32,6 +32,7 @@ func main() {
 	}
 
 	router := gin.Default()
+	router.Use(CORSMiddleware())
 	router.Static("/web", "./www/")
 	router.GET("/", func(c *gin.Context) {
 		location := url.URL{Path: "/web"}
@@ -53,4 +54,15 @@ func main() {
 
 	logger.Infow("Graceful shutdown")
 	_ = logger.Sync()
+}
+
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+
+		c.Next()
+	}
 }
