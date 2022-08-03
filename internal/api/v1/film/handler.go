356b659879d39c050a352d8b755bcfeeafaad4f5
Andrei Khodko <andrei.khodko@gmail.com>
2022-08-03 05:11:43 +0300

    feat: base handler

--- internal/api/v1/film/handler.go
+++ internal/api/v1/film/handler.go
@@ -1,36 +1,54 @@
 package film
 
 import (
-	"github.com/gin-gonic/gin"
-
+	"context"
+	v1 "github.com/HalvaPovidlo/halvabot-go/internal/api/v1"
 	"github.com/HalvaPovidlo/halvabot-go/internal/pkg/item"
+	"github.com/gin-gonic/gin"
 )
 
 type FilmService interface {
-	Add(film *item.Film) (string, error)
-	Comment(filmID string, userID string, comment string)
-	Score(filmID string, userID string, score int)
-	GetFilm(filmID string) *item.Film
+	NewFilm(ctx context.Context, film *item.Film, userID string, withKP bool) (*item.Film, error)
+	NewKinopoiskFilm(ctx context.Context, uri, userID string, score int) (*item.Film, error)
+	EditFilm(ctx context.Context, film *item.Film) (*item.Film, error)
+	AllFilms(ctx context.Context) ([]item.Film, error)
+	Film(ctx context.Context, filmID string) (*item.Film, error)
+	Comment(ctx context.Context, text, filmID, userID string) error
+	Score(ctx context.Context, filmID, userID string, score int) (*item.Film, error)
 }
 
-// Handler TODO: Auth
 type Handler struct {
 	service FilmService
-	super   *gin.RouterGroup
 }
 
 func NewHandler(service FilmService, superGroup *gin.RouterGroup) *Handler {
 	return &Handler{
 		service: service,
-		super:   superGroup,
 	}
 }
 
-func (h *Handler) Router() *gin.RouterGroup {
-	music := h.super.Group("/film")
-	return music
+func (h *Handler) GetFilmsAll(c *gin.Context) {
+	films, err := h.service.AllFilms(c)
+	if err != nil {
+		return
+	}
+}
+func (h *Handler) PostFilmsKinopoisk(c *gin.Context) {
+
+}
+func (h *Handler) PostFilmsNew(c *gin.Context, params v1.PostFilmsNewParams) {
+
+}
+func (h *Handler) GetFilms(c *gin.Context, id v1.FilmId) {
+
+}
+func (h *Handler) PostFilmsId(c *gin.Context, id v1.FilmId) {
+
+}
+func (h *Handler) PostFilms(c *gin.Context, id v1.FilmId) {
+
+}
+func (h *Handler) PostFilmsIdScore(c *gin.Context, id v1.FilmId) {
+
 }
 
-type Response struct {
-	Message string `json:"message"`
-}
